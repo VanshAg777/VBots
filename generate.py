7b682036e187e18a8764fe8f67d2f896c45e0dd5
VanshAg777 <114874910+VanshAg777@users.noreply.github.com>
2023-01-15 17:46:25 -0600

    Task F midpoint

--- generate.py
+++ generate.py
@@ -1,33 +1,67 @@
 import pyrosim.pyrosim as pyrosim
 
-pyrosim.Start_SDF("boxes.sdf")
-length = 1
-width = 1
-height = 1
-x = 0
-y = 0
-z = 0.5
+def Create_World():
 
-for i in range(0,5):
-    for j in range(0,5):
+    pyrosim.Start_SDF("world.sdf")
+    length = 1
+    width = 1
+    height = 1
+    x = -3
+    y = 3
+    z = 0.5
 
-        for k in range(0,10):
+    # for i in range(0,5):
+    #     for j in range(0,5):
+    #         for k in range(0,10):
+    #             pyrosim.Send_Cube(name="Box", pos=[x,y,z+k] , size=[length,width,height])
+    #             length = length * 0.9
+    #             width = width * 0.9
+    #             height = height * 0.9
+    #         y = y+1
+    #         z = 0.5
+    #         length = 1
+    #         width = 1
+    #         height = 1
+    #     y = 0
+    #     x = x+1
 
-            pyrosim.Send_Cube(name="Box", pos=[x,y,z+k] , size=[length,width,height])
-            length = length * 0.9
-            width = width * 0.9
-            height = height * 0.9
-        y = y+1
-        z = 0.5
-        length = 1
-        width = 1
-        height = 1
+    pyrosim.Send_Cube(name="Box", pos=[x,y,z] , size=[length,width,height])
+    # pyrosim.Send_Cube(name="Box2", pos=[x,1,1.5] , size=[length,width,height])
+
+    pyrosim.End()
+
+def Create_Robot():
+    pyrosim.Start_URDF("body.urdf")
+
+    length = 1
+    width = 1
+    height = 1
+    x = 0
     y = 0
-    x = x+1
+    z = 0.5
+
+    # slide bot
+    # pyrosim.Send_Cube(name="Link0", pos=[x,y,z] , size=[length,width,height])
+    # pyrosim.Send_Joint( name = "Link0_Link1" , parent= "Link0" , child = "Link1" , type = "revolute", position = [0,0,1])
+    # pyrosim.Send_Cube(name="Link1", pos=[0,0,0.5] , size=[length,width,height])
+
+    # pyrosim.Send_Joint( name = "Link1_Link2" , parent= "Link1" , child = "Link2" , type = "revolute", position = [0,0,1])
+    # pyrosim.Send_Cube(name="Link2", pos=[0,0,0.5] , size=[length,width,height])
+
+    # pyrosim.Send_Joint( name = "Link2_Link3" , parent= "Link2" , child = "Link3" , type = "revolute", position = [0,0.5,0.5])
+    # pyrosim.Send_Cube(name="Link3", pos=[0,0.5,0] , size=[length,width,height])
+
+
+    pyrosim.Send_Cube(name="Torso", pos=[1.5,y,1.5] , size=[length,width,height])
+    pyrosim.Send_Joint( name = "Torso_BackLeg" , parent= "Torso" , child = "BackLeg" , type = "revolute", position = [1,0,1])
+    pyrosim.Send_Cube(name="BackLeg", pos=[-0.5,0,-0.5] , size=[length,width,height])
+
+    pyrosim.Send_Joint( name = "Torso_FrontLeg" , parent= "Torso" , child = "FrontLeg" , type = "revolute", position = [2,0,1])
+    pyrosim.Send_Cube(name="FrontLeg", pos=[0.5,0,-0.5] , size=[length,width,height])
+    pyrosim.End()
 
 
 
-# pyrosim.Send_Cube(name="Box2", pos=[x,1,1.5] , size=[length,width,height])
 
-
-pyrosim.End()
+Create_World()
+Create_Robot()
--- simulate.py
+++ simulate.py
@@ -1,6 +1,10 @@
 import pybullet as p
 import time
 import pybullet_data
+import pyrosim.pyrosim as pyrosim
+import numpy 
+#from data import TemporaryFile
+
 
 
 physicsClient = p.connect(p.GUI)
@@ -8,14 +12,24 @@ physicsClient = p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
 p.setGravity(0,0,-9.8)
 planeId = p.loadURDF("plane.urdf")
+robotId = p.loadURDF("body.urdf")
 
-p.loadSDF("boxes.sdf")
+p.loadSDF("world.sdf")
 
-for i in range(0,1000):
+pyrosim.Prepare_To_Simulate(robotId)
+
+backLegSensorValues = numpy.zeros(100)
+
+for i in range(0,100):
     p.stepSimulation()
+    backLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
+    # backLegTouch = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
+    # print(backLegTouch)
     time.sleep( 1/60 )
-    print(i)
-
+    # print(i)
 
 
 p.disconnect()
+print(backLegSensorValues)
+
+numpy.save("./data/backLegSensorValues.npy", backLegSensorValues)
