cd52de56962a3211b7418415169ac71fccf84603
VanshAg777 <114874910+VanshAg777@users.noreply.github.com>
2023-01-15 19:32:50 -0600

    Changes in Pyrosim.py

--- simulate.py
+++ simulate.py
@@ -27,6 +27,13 @@ for i in range(0,100):
     backLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
     frontLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("FrontLeg")
 
+    pyrosim.Set_Motor_For_Joint(
+    bodyIndex = robotId,
+    jointName = "Torso_BackLeg",
+    controlMode = p.POSITION_CONTROL,
+    targetPosition = 0.0,
+    maxForce = 500)
+
 
     # backLegTouch = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
     # print(backLegTouch)
