0aede95ed217cd371189fe1fb31370c781df65d9
VanshAg777 <114874910+VanshAg777@users.noreply.github.com>
2023-01-22 14:11:26 -0600

    Task I completed

--- motor.py
+++ motor.py
@@ -7,19 +7,19 @@ import constants as c
 class MOTOR:
     def __init__(self, jointName):
         self.jointName = jointName
-        self.Prepare_To_Act()
+        # self.Prepare_To_Act()
 
-    def Prepare_To_Act(self):
-        self.amplitude = c.amplitudeBackLeg
+    # def Prepare_To_Act(self):
+    #     self.amplitude = c.amplitudeBackLeg
 
-        if self.jointName == "Torso_FrontLeg":
-            self.frequency = c.frequencyBackLeg/2
-        else:
-            self.frequency = c.frequencyBackLeg
+    #     if self.jointName == "Torso_FrontLeg":
+    #         self.frequency = c.frequencyBackLeg/2
+    #     else:
+    #         self.frequency = c.frequencyBackLeg
         
-        self.offset = c.phaseOffsetBackLeg
+    #     self.offset = c.phaseOffsetBackLeg
 
-        self.targetAnglesBackLeg = self.amplitude  * numpy.sin(self.frequency  * numpy.linspace(0, 2*(numpy.pi), c.simLength) + self.offset)
+    #     self.targetAnglesBackLeg = self.amplitude  * numpy.sin(self.frequency  * numpy.linspace(0, 2*(numpy.pi), c.simLength) + self.offset)
 
     def Set_Value(self, robotId, desiredAngle):
         pyrosim.Set_Motor_For_Joint(
@@ -29,7 +29,7 @@ class MOTOR:
         targetPosition = desiredAngle,
         maxForce = c.maxForceBackLeg)
 
-    def Save_Values(self):
-        # numpy.save("./data/targetAnglesBackLeg.npy", targetAnglesBackLeg)
-        # numpy.save("./data/targetAnglesFrontLeg.npy", targetAnglesFrontLeg)
-        pass
+    # def Save_Values(self):
+    #     # numpy.save("./data/targetAnglesBackLeg.npy", targetAnglesBackLeg)
+    #     # numpy.save("./data/targetAnglesFrontLeg.npy", targetAnglesFrontLeg)
+    #     pass
--- robot.py
+++ robot.py
@@ -32,7 +32,7 @@ class ROBOT:
                 jointName = self.nn.Get_Motor_Neurons_Joint(neuronName)
                 desiredAngle = self.nn.Get_Value_Of(neuronName)
                 self.motors[jointName].Set_Value(self.robotId, desiredAngle)
-                print(neuronName + " " + jointName + " " + str(desiredAngle))
+                # print(neuronName + " " + jointName + " " + str(desiredAngle))
 
 
 
