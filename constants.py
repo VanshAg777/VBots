ecbddf3df72604ab3257addb9a43fdb545d5371d
VanshAg777 <114874910+VanshAg777@users.noreply.github.com>
2023-01-30 20:49:56 -0600

    Task N stuck

--- constants.py
+++ constants.py
@@ -11,20 +11,22 @@ phaseOffsetBackLeg = 0
 
 # phaseOffsetBackLeg = numpy.pi/7
 
-maxForceBackLeg = 22
+maxForceBackLeg = 23
 
 amplitudeFrontLeg = numpy.pi/3
 frequencyFrontLeg = 10
 phaseOffsetFrontLeg = 0
 
-maxForceFrontLeg = 22
+maxForceFrontLeg = 23
 
-numberOfGenerations = 1
+numberOfGenerations = 10
 
-populationSize = 1
+populationSize = 10
 
-numSensorNeurons = 3
-numMotorNeurons = 2
+numSensorNeurons = 4
+numMotorNeurons = 8
+
+motorJointRange = 0.8
 
 # targetPosition = -(numpy.pi)/4,
 # targetPosition = random.uniform(-(numpy.pi)/2,(numpy.pi)/2),
